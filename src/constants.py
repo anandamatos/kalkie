5d62baf38a9edbcc489f5cdf30c22615062f3eeb
Ananda Matos <anandamatos@gmail.com>
2025-10-30 19:14:46 -0300

    ajustes

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,12 @@
 # src/constants.py
 import numpy as np
 
-# CONSTANTES BÁSICAS (mantenha as que já existem)
-KCAL_KARATE = 1000
-KCAL_CALISTENIA = 300
-KCAL_CORRIDA = 100
-KCAL_BIKE = KCAL_CORRIDA / 3
-BASAL = 2300
+# CONSTANTES BÁSICAS
+KCAL_KARATE = 1000 # 2h de treino = 8KU (KU = Karate Units = 15min de treino moderado) 
+KCAL_CALISTENIA = 300 # por 30min de treino moderado / 20x = 6min + 6 alongamento
+KCAL_CORRIDA = 100 # por km = 12min de corrida 
+KCAL_BIKE = KCAL_CORRIDA # 3 # por 2km = 3min por km
+BASAL = 2000
 KCAL_POR_KG = 7000
 AGUA_Q10 = 5000
-MAX_DEFICIT_ALIMENTAR = 1300
-
-# REMOVA a configuração complexa de quadrantes por enquanto
-# Vamos mover isso para um módulo separado depois
+MAX_DEFICIT_ALIMENTAR = 1000 # 50% do Plano alimentar
